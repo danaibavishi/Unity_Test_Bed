be6feeb14fbc7714a1bdc1f2b8f21fbbca630305
Danai Bavishi <danaibavishi@gmail.com>
2020-01-29 11:37:05 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Unity_Test_Bed
+ SImple Unity game used to demonstrate the possible implementations of ML model outputs
---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    public OSC osc;
+
+    private Rigidbody rb;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        osc.SetAddressHandler("/Finger_point", Jump);
+    }
+
+	void FixedUpdate ()
+	    {
+	        float moveHorizontal = Input.GetAxis("Horizontal");
+	        float moveVertical = Input.GetAxis ("Vertical");
+	   
+	        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+	   
+	        GetComponent<Rigidbody>().AddForce (movement * speed * Time.deltaTime);
+	 
+	    }
+   	void Jump (OscMessage message)
+   		{
+   		    Vector3 jump = new Vector3 (0.0f, 200.0f, 0.0f);
+	        GetComponent<Rigidbody>().AddForce (jump);
+   		    }
+  }
+    
+
