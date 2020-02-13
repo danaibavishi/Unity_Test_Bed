4f96a67fefcd8e1629181d8f603e12eea57a4dab
danaibavishi <danaibavishi@gmail.com>
2020-02-13 15:56:42 -0600

    Finger tip coordinates extracted

---
+++ LEAP data processing/Assets/LeapMotion/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    public OSC osc;
+    bool blue;
+    private Rigidbody rb;
+
+    void Start ()
+    {
+        blue = true;
+        rb = GetComponent<Rigidbody>();
+        osc.SetAddressHandler("/Cleaning_wave", Jump);
+        osc.SetAddressHandler("/Finger_point", Change_color);
+    }
+
+	void FixedUpdate ()
+	    {
+	        float moveHorizontal = Input.GetAxis("Horizontal");
+	        float moveVertical = Input.GetAxis ("Vertical");
+	   
+	        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+	   
+	        GetComponent<Rigidbody>().AddForce (movement * speed * Time.deltaTime);
+	 
+	    }
+   	void Jump (OscMessage message)
+   		{
+   		    Vector3 jump = new Vector3 (0.0f, 200.0f, 0.0f);
+	        GetComponent<Rigidbody>().AddForce (jump);
+   		    }
+    void Change_color(OscMessage message)
+    {
+        if (blue)
+        {
+            GetComponent<Renderer>().material.color = new Color(0, 0, 0);
+            blue = false;
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = new Color(0, 255, 0);
+            blue = true;
+        }
+    }
+}
+    
+
